e749b544e7804e90ac0df9901dfddf5d3ede5a02
Adriana Quintero <aquintero86@gmail.com>
2021-03-30 14:12:11 -0500

    fix host

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def teardown_appcontext(response_or_exc):
 
 if __name__ == '__main__': 
     init_db()
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
